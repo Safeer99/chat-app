06942a56012c67fea244ad0a9abe14c7b6afba6f
Safeer <safeeranwar123@gmail.com>
2023-01-29 14:31:16 +0530

    create sidebar UI

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" id="name" placeholder="find a user" />
+      </div>
+      <div className="userChat">
+        <img src="" alt="" />
+        <div className="userChatInfo">
+          <span>safeer</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Search
